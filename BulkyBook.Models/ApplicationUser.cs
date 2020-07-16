511729c6b483a6bd3b9f8878ab62eb16258d0714
de-elebit <de.elebit@gmail.com>
2020-07-17 01:59:17 +0900

    Add Role / Change Register Feature

--- BulkyBook.Models/ApplicationUser.cs
+++ BulkyBook.Models/ApplicationUser.cs
@@ -12,6 +12,7 @@ namespace BulkyBook.Models
         [Required]
         public string Name { get; set; }
         public string StreetAddress { get; set; }
+        public string State { get; set; }
         public string City { get; set; }
         public string PostalCode { get; set; }
 
